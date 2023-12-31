d1d6ddce6a590798859d53a6465567b7f107bc52
rajgaurav00 <rajg25412@gmail.com>
2023-12-31 19:45:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// var const let-------------------
+// const dulha = "parrot";
+// const dulhan = "eagle";
+// console.log(dulha + " weds " + dulhan)
+
+// hoisting-------------------
+// console.log(a)
+// var a = 12;
+
+// types in js ___________________
+// prmitive  and refrence [ ] , ()
+
+// conditional statement_______________
+// if else else-if 
+
+// loop ----------------------For loop
+// 4 - 24 
+// for(var a=4; a<25; a++){
+//     console.log(a)
+// } 
+// while loop _____
+var a = 4
+while(a<20;){
+    a++;
+}
+
+
